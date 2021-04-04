f2690f36b45b92affb644ec609b889562f444d23
div5252 <54735797+div5252@users.noreply.github.com>
2021-04-04 13:41:57 +0530

    Create task2.cpp

---
+++ task2/task2.cpp
@@ -0,0 +1,33 @@
+template <typename T, int N>
+class small_vector : public small_vector_impl<T>
+{
+    private:
+        char buffer[sizeof(T) * N];
+
+    public:
+        small_vector() : small_vector_impl((T *)buffer, (T *)buffer, N) {}
+
+        // ...
+}
+
+template <typename T>
+class small_vector_impl
+{
+    private:
+        T *_begin, *_end;
+        size_t capacity;
+
+    public:
+        small_vector_impl(T *_begin, T *_end, size_t capacity);
+        
+        iterator begin()
+        {
+            return _begin;
+        }
+        iterator end()
+        {
+            return _end;
+        }
+
+        // ...
+}
